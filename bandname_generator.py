c670bd9bdc8206740f5e47f6a3c50cfacc1846ce
kaushik567321 <122045410+kaushik567321@users.noreply.github.com>
2023-01-05 23:37:39 +0530

    Add files via upload

---
+++ bandname_generator.py
@@ -0,0 +1,5 @@
+print("welcome to the band name Generator")
+city=input("which did you grew up?")
+species=input("what is your favorite animal species")
+print("your band name could be " + " " + city + " " + species)
+
